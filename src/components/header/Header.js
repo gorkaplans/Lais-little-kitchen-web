02c108afcbc65128da218dcfb82657a747c9e769
Gorka Plans <gorkaplans@gmail.com>
2021-05-07 18:07:40 +0200

    Home-resposive

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'; 
+import React, { useState }from 'react'; 
 import { Link } from 'react-router-dom';
 
 import'./Header.scss'
@@ -7,16 +7,28 @@ import'./Header.scss'
 
 
 const Header = () => {
+
+    const[activo, setActivo]= useState(false)
+
+    const handleMenuHam = () =>{
+        setActivo(!activo)
+        console.log('Hola Mundo')
+    }
     
     return (
     <section className="section">
         <div className="container">
-            <Link className="heading2-red" to='/'>Lai's little kitchen</Link>
-            <div className="nav-menu">
-                <Link className="text-blue nav-item" to='/About'>Sobre mi</Link>
-                <Link className="text-blue nav-item" to='/Recetas'>Recetas</Link>
-                <Link className="text-blue nav-item" to='/Contact'>Contacto</Link>
+            <Link className="heading2-red mobile-logo" to='/'>Lai's little kitchen</Link>
+            <div className={(activo) ? "nav-menu activado" : "nav-menu descativado"}>
+                    <Link className="text-blue nav-item" to='/About'>Sobre mi</Link>
+                    <Link className="text-blue nav-item" to='/Recetas'>Recetas</Link>
+                    <Link className="text-blue nav-item" to='/Contact'>Contacto</Link>
             </div>
+            <button className="ham" type="button" onClick={handleMenuHam}> 
+                <span className="br-1"></span>
+                <span className="br-2"></span>
+                <span className="br-3"></span>
+            </button>
         </div>
     </section>
     
