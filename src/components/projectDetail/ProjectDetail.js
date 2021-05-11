4ff72a6b4cdbeaf7db31034db06b84072abeface
Gorka Plans <gorkaplans@gmail.com>
2021-05-11 20:27:47 +0200

    fsdfsd

--- src/components/projectDetail/ProjectDetail.js
+++ src/components/projectDetail/ProjectDetail.js
@@ -1,63 +1,77 @@
-import React, { useEffect, useState } from 'react'; 
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import marked from 'marked'
+
 
 import { client } from '../../client'
 
 
-import'./ProjectDetail.scss'
+
+import './ProjectDetail.scss'
 
 
 
 
 const ProjectDetail = () => {
-    const[receta, setReceta] = useState({})
-    const[image, setImage] = useState('')
-    const[ingredientes, setIngredientes] = useState(['2','3'])
+    const [receta, setReceta] = useState({})
+    const [image, setImage] = useState('')
+    const [ingredientes, setIngredientes] = useState(['2', '3'])
+    const [description, setDescription] = useState('')
 
 
     let { id } = useParams()
+/*     const formatDescription = marked(description) */
+
 
     async function getSingleReceta(idReceta) {
         const entry = await client.getEntry(idReceta)
         setReceta(entry.fields)
         setImage(entry.fields.image.fields.file.url)
         setIngredientes(entry.fields.ingredientes)
+        setDescription('puta')
     }
 
     useEffect(() => {
-            getSingleReceta(id)
+        getSingleReceta(id)
     }, [])
-    
+
     console.log(receta)
 
     return (
         <div className="section">
-            <div className="container">
+            <div className="container container-detail">
                 <div className="detail-hero">
                     <h1 className="heading">{receta.name}</h1>
-                    <div className="index-detail">
+                    <img className="detail-img" src={image}></img>
+                </div>
+                <div className="description-container">
+                    <span>
+                        <h1 className="heading2-blue title-index"> Ingredientes</h1>
+                        {<ul className="text-blue ingredients-wrapper">
+
+                            {ingredientes.map((ingrediente, i) =>
+                                <>
+                                    <li className="ingredient-item">· {ingrediente}</li>
+                                </>
+                            )}
+                        </ul>}
                         <span className="index-item-wrapper">
-                            <h1 className="heading2-blue title-index"> Dificultad</h1>
+                            <h1 className="heading3-blue title-index"> Dificultad</h1>
                             <p className="text-blue">{receta.dificult}</p>
-                            <h1 className="heading2-blue title-index"> Tiempo</h1>
+                        </span>
+                        <span>
+                            <h1 className="heading3-blue title-index"> Tiempo</h1>
                             <p className="text-blue">{receta.time}</p>
                         </span>
-                         <span>
-                            <h1 className="heading2-blue title-index"> Ingredientes</h1>
-                            {<ul className="text-blue ingredients-wrapper"> 
-                                
-                                {ingredientes.map((ingrediente, i) =>
-                                <>
-                                <li className="ingredient-item">· {ingrediente}</li>
-                                </>
-                                )}
-                             </ul>}
-                        </span> 
-                    </div>
-                      <img className="detail-img" src={image}></img>
-                </div>            
+                    </span>
+                    <span>
+                        <h1 className="heading2-blue title-index">Preparación</h1>
+                        <p className="text-blue">{description}</p>
+                    </span>
+                </div>
             </div>
         </div>
-    
-    ) } 
- export default ProjectDetail;
+
+    )
+}
+export default ProjectDetail;
